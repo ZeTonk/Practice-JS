bb03d19219f0496eba9940d98d680a773529a396
ZeTonk <demian.english@outlook.com>
2024-11-20 13:05:01 -0700

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,37 @@
+let num=50;
+if(num >= 50)
+{
+    console.log("Shipping fee 8$ will be charged")
+}
+else(num < 50)
+{
+    console.log("Free Shipping")
+}
+let userName = "John123";
+if(userName === "John123"){
+    console.log("Welcome to Shopping Spree")
+}
+else{
+    console.log("username does not exist")
+}
+let method="Credit Card";
+if(method === "Credit Card"){
+    console.log("You have chosen to pay with a credit card")
+}
+else{
+    console.log("You have chosen not to use a credit card. Here are some alternative payment methods: PayPal, Bank Transfer, or Cash.")
+}
+let gamemode = "MultiPlayer";
+if(gamemode === "MultiPlayer"){
+    console.log("Voice-Chat Enabled")
+}
+else{
+    console.log("Single-Player Mode")
+}
+let number = 1000;
+if(number >= 1000){
+    console.log("You have unlocked the next level")
+}
+else{
+    console.log("Keep playing to unlock the next level")
+}
